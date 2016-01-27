2a190c96e998bf91064aa1429b719a871ec991f4
Blair Peterson and Will Murphy <wilmurph@gmail.com>
2016-01-27 10:24:21 -0800

    add WordBank spec and passing object

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,4 @@
-function Game(player1,player2) {
 
-}
-
-function Board() {
-  this.boardArray = [];
-
-  for (var index=0; index < 9; index++) {
-    this.boardArray.push(Math.pow(2,index));
-  }
-}
-
-function Player(mark) {
-  this.mark = mark;
+function WordBank() {
+  this.wordArray = ["candy bean","ninja","superstar"];
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,27 +1,6 @@
-describe("Board", function() {
-  it("creates 9 spaces when it is initialized", function() {
-    var testBoard = new Board();
-    expect(testBoard.boardArray).to.eql([1,2,4,8,16,32,64,128,256]);
+describe("WordBank", function() {
+  it("creates an array to store words for game", function() {
+    var testWordBank = new WordBank();
+    expect(testWordBank.wordArray).to.eql(["candy bean","ninja","superstar"]);
   });
 });
-
-describe("Player", function() {
-  it("returns the player's mark", function() {
-    var testPlayer = new Player("X");
-    expect(testPlayer.mark).to.equal("X");
-  });
-});
-
-// describe("Space", function() {
-//   it("returns the player's mark", function() {
-//     var testSpace = new Space();
-//     expect(testPlayer.mark).to.equal("X");
-//   });
-// });
-
-describe("Game", function() {
-  it("creates two players", function() {
-    var testGame = new Game(player1,player2);
-    expect(testGame.player1).to.equal()
-  })
-})
